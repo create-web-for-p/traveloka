663e5e185b7c098f085d49d8ca3e690fe07a34be
worawut srimala <worawut48@hotmail.co.th>
2022-01-05 13:38:04 +0700

    frist meet

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,6 @@
 <template>
   <div>
-      <h3 data-testid="title">{{ title }}</h3>
-       <div>
-      Username: <input type="text" name="username" id="username"
-        v-model="username" data-testid="username">
-    </div>
-    <div>
-      Password: <input type="password" name="password" id="password"
-        v-model="password" data-testid="password">
-    </div>
-     <button @click="summit">submit</button>
-    <p data-testid="Times">Times clicked: {{ count }}</p>
-    <button @click="increment">increment</button>
-      <input  data-testid="click" type="text" id="fname" name="fname"><br><br>
+    <h1>Hello</h1>
   </div>
 </template>
 
